ff5270259200b9a9710cf34429c58ae635e77fe2
Jami <jamsheduddin03@gmail.com>
2023-03-31 11:42:03 +0600

    blog component added

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header/Header";
+import Main from "./components/Main/Main";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -10,6 +11,7 @@ function App() {
     <div className="App">
       <div className="w-3/4 m-auto">
         <Header></Header>
+        <Main></Main>
       </div>
     </div>
   );
---
+++ src/components/Blog/Blog.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBookmark } from "@fortawesome/free-regular-svg-icons";
+
+const Blog = (props) => {
+  const {
+    id,
+    authorName,
+    blogTitle,
+    coverImage,
+    authorImage,
+    readTime,
+    publishDate,
+  } = props.blog;
+  console.log(authorImage);
+  return (
+    <div className="my-2">
+      <div>
+        <img className=" rounded-lg" src={coverImage} alt="" />
+      </div>
+      <div className="flex justify-between my-4">
+        <div className="flex gap-2 cursor-pointer">
+          <div>
+            <img className="w-10 rounded-full" src={authorImage} alt="" />
+          </div>
+          <div>
+            <p className="text-lg font-bold">{authorName}</p>
+            <p>{publishDate}</p>
+          </div>
+        </div>
+        <div>
+          {readTime} minute read{" "}
+          <span className="cursor-pointer text-xl px-3">
+            <FontAwesomeIcon icon={faBookmark} />
+          </span>
+        </div>
+      </div>
+      <div>
+        <h1 className="text-4xl font-bold mb-3">{blogTitle}</h1>
+      </div>
+      <div className="mb-2">
+        <p>#beginner #programming</p>
+      </div>
+      <div>
+        <button className="text-blue-700 hover:text-black font-semibold underline">
+          Mark as read
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Blog;
